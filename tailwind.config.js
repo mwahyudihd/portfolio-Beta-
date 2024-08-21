d024bd25b186d62b1418fe39149fbc959dd47842
M Wahyudi HD <muh.wahyudi282828@gmail.com>
2024-08-21 23:41:19 +0700

    build project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary : '#65451F',
+        second : '#765827',
+        coffee : '#C8AE7D',
+        coffeeLight : '#EAC696',
+        cafe : '#C38154',
+        darkCoffee: '#252525',
+        semiDark: '#87805E'
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
+
