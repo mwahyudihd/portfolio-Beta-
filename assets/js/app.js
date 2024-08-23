bc7dc7b11a92e8b491d5dd6b4515d7ec8a8dfdcb
M Wahyudi HD <muh.wahyudi282828@gmail.com>
2024-08-23 20:14:13 +0700

    feat: build scroll animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,29 @@
 const burgerBtn = document.querySelector('#hamburger');
+const navbar = document.querySelector('header');
+const navFix = document.querySelector('#nav-extra');
 
 burgerBtn.addEventListener('click',() => {
     burgerBtn.classList.toggle("hamburger-actived");
 });
+
+let currentScroll;
+
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    console.log(currentScroll);
+    
+    if( currentScroll < 100 ) {
+        navbar.classList.add('absolute');
+        navFix.classList.add('hidden');
+        navbar.classList.remove('backdrop-blur-nav');
+    } else if ( currentScroll > scroll ){
+        navbar.classList.add('backdrop-blur-nav');
+        navbar.classList.remove('absolute');
+        navFix.classList.remove('hidden');
+    } else {
+        navbar.classList.remove('backdrop-blur-nav');
+        navFix.classList.add('hidden');
+    }
+    
+    currentScroll = scroll;
+})
---
+++ assets/js/gsap-set.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger,TextPlugin)
+    // gsap code here!
+    gsap.from('#title',{
+        x: -100, duration: 3
+    });
+    gsap.from('svg', {
+        scale: -0,
+        opacity: 0,
+        duration: 2,
+        scrollTrigger: {
+            trigger: 'svg',
+            start: "top 70%",
+            end: "top 15%",
+            toggleActions: "restart reverse restart reverse",
+            // markers: {
+            //     startColor: '#252525',
+            //     endColor: 'red',
+            //     fontSize: '4rem',
+            //     indent: 200
+            // }
+        }
+    });
+    gsap.from('img', {
+        y: 100,
+        opacity: 0,
+        duration: 3,
+        scrollTrigger: {
+            trigger: 'img',
+            start: "top 70%",
+            end: "top 15%",
+            toggleActions: "restart reverse restart reverse",
+            // markers: {
+            //     startColor: '#252525',
+            //     endColor: 'red',
+            //     fontSize: '4rem',
+            //     indent: 200
+            // }
+        }
+    })
+});
