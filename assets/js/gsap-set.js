49055f460fbaeda0997e1c9a35db3f9ccd4ab5eb
M Wahyudi HD <muh.wahyudi282828@gmail.com>
2024-08-23 22:53:58 +0700

    style: adding Animations

--- assets/js/gsap-set.js
+++ assets/js/gsap-set.js
@@ -1,14 +1,36 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger,TextPlugin);
     gsap.from('#title',{
-        x: -100, duration: 3
+        x: -200, duration: 2,
+        scrollTrigger: {
+            trigger: '#title',
+            start: "top 61%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+        }
+    });
+    gsap.to("#icon",{
+        text: 'Wahyudi',
+        duration: 2,
+        delay: 1
+    });
+    gsap.to('#child-title', {
+        text: `<span class="text-base md:text-xl lg:text-2xl text-primary">I'm</span>Wahyudi`,
+        duration: 2,
+        delay: 2,
+        scrollTrigger: {
+            trigger: '#child-title',
+            start: "top 70%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+        }
     });
     gsap.from('#blob', {
         scale: -0,
         opacity: 0,
         duration: 2,
         scrollTrigger: {
-            trigger: 'svg',
+            trigger: '#blob',
             start: "top 70%",
             end: "top 15%",
             toggleActions: "restart reverse restart reverse",
@@ -36,5 +58,64 @@ document.addEventListener("DOMContentLoaded", (event) => {
             //     indent: 200
             // }
         }
+    });
+    gsap.from('#first', {
+        x: -200,
+        opacity: 0,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+            trigger: '#first',
+            start: "top 70%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+        }
+    });
+    gsap.from('#middle', {
+        scale: -0,
+        duration: 2,
+        scrollTrigger: {
+            trigger: '#middle',
+            start: "top 70%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+        }
     })
+    gsap.from('#last', {
+        x: 200,
+        opacity: 0,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+            trigger: '#last',
+            start: "top 70%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+            
+        }
+    });
+    gsap.to('#motto',{
+        text: `Live is a<span class="text-primary font-bold"> Code</span>, Break Down Complexities, Find the Patterns, and Build Solutions.`,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+            trigger: '#motto',
+            start: "top 81%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+            
+        }
+    });
+    gsap.from('#btn-contact-me', {
+        opacity: 0,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+            trigger: '#btn-contact-me',
+            start: "top 85%",
+            end: "top 10%",
+            toggleActions: "restart reverse restart reverse",
+            
+        }
+    });
 });
